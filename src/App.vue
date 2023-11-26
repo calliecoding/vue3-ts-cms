f950c9c0ab825489bd01085543097085c1470cc8
callie <chzx_dqy@outlook.com>
2023-11-26 21:50:11 +0800

    perf: element-plus按需导入配置

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,21 @@
   <router-link to="/login">登录</router-link>
   <router-link to="/main">首页</router-link>
   <router-view></router-view>
+  <el-button>Default</el-button>
   <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { ElButton } from 'element-plus'
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: { ElButton }
 })
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,35 @@
 const path = require('path')
+// webpack.config.js
+const AutoImport = require('unplugin-auto-import/webpack').default
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
   // 1.配置方式一: CLI提供的属性
   outputDir: './build',
   publicPath: './',
   // 2.配置方式二: 和webpack属性完全一致, 最后会进行合并
-  // configureWebpack: {
-  //   resolve: {
-  //     alias: {
-  //       components: '@/components'
-  //     }
-  //   }
-  // },
-  // configureWebpack: (config) => {
-  //   config.resolve.alias = {
-  //     '@': path.resolve(__dirname, 'src'),
-  //     components: '@/components'
-  //   }
-  // }
+  configureWebpack: {
+    //   resolve: {
+    //     alias: {
+    //       components: '@/components'
+    //     }
+    //   }
+    // },
+    // configureWebpack: (config) => {
+    //   config.resolve.alias = {
+    //     '@': path.resolve(__dirname, 'src'),
+    //     components: '@/components'
+    //   }
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ]
+  },
   // 3.配置方式三:
   chainWebpack: (config) => {
     config.resolve.alias
