9854ded076c5f72557a1850c28b07460c528fa91
callie <chzx_dqy@outlook.com>
2023-11-26 16:22:54 +0800

    test: element-plus全局引入和使用

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <router-link to="/login">登录</router-link>
   <router-link to="/main">首页</router-link>
   <router-view></router-view>
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <script lang="ts">
