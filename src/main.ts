18c3da7edbb28a236556897a7c76fcea8a8b59f1
callie <chzx_dqy@outlook.com>
2023-11-26 21:59:15 +0800

    perf: 全局注册高频使用的某组件

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ElButton, ElRow } from 'element-plus'
 
 import router from './router'
 import store from './store'
 
 const app = createApp(App)
 
+const components = [ElButton, ElRow]
+for (const component of components) {
+  app.component(component.name, component)
+}
 app.use(router)
 app.use(store)
-
 app.mount('#app')
