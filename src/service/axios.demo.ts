f17f10825d9e3aa0f82b59b2278e141c4a72d126
callie <chzx_dqy@outlook.com>
2023-11-27 23:10:13 +0800

    feat(service): axios配置选项-all-拦截器

--- src/service/axios.demo.ts
+++ src/service/axios.demo.ts
@@ -9,24 +9,72 @@ import axios, { AxiosResponse } from 'axios'
 //   })
 
 // 2.模拟get请求,并且传入参数
-axios
-  .get('http://httpbin.org/get', {
-    params: {
-      name: 'cc',
-      age: 18
-    }
-  })
-  .then((res) => {
-    console.log(res.data)
-  })
+// axios
+//   .get('http://httpbin.org/get', {
+//     params: {
+//       name: 'cc',
+//       age: 18
+//     }
+//   })
+//   .then((res) => {
+//     console.log(res.data)
+//   })
 // 3.模拟post请求
+// axios
+//   .post('http://httpbin.org/post', {
+//     data: {
+//       name: 'cc',
+//       age: 18
+//     }
+//   })
+//   .then((res) => {
+//     console.log(res.data)
+//   })
+
+// 4.axios的配置选项
+// 4.1. 全局的配置
+axios.defaults.baseURL = 'http://httpbin.org'
+axios.defaults.timeout = 10000
+// axios.defaults.headers = {}
+
+// 4.2. 每一个请求单独的配置
+// axios
+//   .get('/get', {
+//     params: {
+//       name: 'coderwhy',
+//       age: 18
+//     },
+//     timeout: 5000,
+//     headers: {}
+//   })
+//   .then((res) => {
+//     console.log(res.data)
+//   })
+
+// 5.axios.all -> 多个请求, 一起返回
 axios
-  .post('http://httpbin.org/post', {
-    data: {
-      name: 'cc',
-      age: 18
-    }
-  })
+  .all([
+    axios.get('/get', { params: { name: 'why', age: 18 } }),
+    axios.post('/post', { data: { name: 'why', age: 18 } })
+  ])
   .then((res) => {
-    console.log(res.data)
+    console.log(res[0].data)
+    console.log(res[1].data)
   })
+
+// 6.axios的拦截器
+// fn1: 请求发送成功会执行的函数
+// fn2: 请求发送失败会执行的函数
+axios.interceptors.request.use(
+  (config) => {
+    // 想做的一些操作
+    // 1.给请求添加token
+    // 2.isLoading动画
+    console.log('请求成功的拦截')
+    return config
+  },
+  (err) => {
+    console.log('请求发送错误')
+    return err
+  }
+)
