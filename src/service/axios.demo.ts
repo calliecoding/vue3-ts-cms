4f96364e6cd88a004b6d5beb6007987ffc8b9d26
callie <chzx_dqy@outlook.com>
2023-11-27 22:50:23 +0800

    feat(service): axios的get和post请求使用

--- src/service/axios.demo.ts
+++ src/service/axios.demo.ts
@@ -1,12 +1,32 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 // axios的实例对象
-axios.get('http://123.207.32.32:8000/home/multidata').then((res) => {
-  console.log(res.data)
-})
+// 1.模拟get请求
+// axios
+//   .get('http://123.207.32.32:8000/home/multidata')
+//   .then((res: AxiosResponse) => {
+//     console.log(res.data)
+//   })
 
-new Promise<string>((resolve) => {
-  resolve('哈哈')
-}).then((res) => {
-  console.log(res)
-})
+// 2.模拟get请求,并且传入参数
+axios
+  .get('http://httpbin.org/get', {
+    params: {
+      name: 'cc',
+      age: 18
+    }
+  })
+  .then((res) => {
+    console.log(res.data)
+  })
+// 3.模拟post请求
+axios
+  .post('http://httpbin.org/post', {
+    data: {
+      name: 'cc',
+      age: 18
+    }
+  })
+  .then((res) => {
+    console.log(res.data)
+  })
