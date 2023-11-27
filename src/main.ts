79a14fe8e29db18223171f48e8cf9e0381e065ea
callie <chzx_dqy@outlook.com>
2023-11-27 15:45:44 +0800

    style: 注释优化

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,10 @@ import store from './store'
 import { globalRegister } from '@/global/index'
 
 const app: App = createApp(rootApp)
-// registerApp(app)
 
-// //app.use支持传入一个函数
-// app.use(function (app: App) {})
-// //app.use支持传入一个对象，并在执行时默认调用install函数
-// app.use({
-//   install: function (app: App) {}
-// })
+//注册element-plus和其它
+app.use(globalRegister)
 
-/* 注册element-plus和其它 */
-app.use(globalRegister) //app. use 内部执行时，会执行传入的函数并给这个函数传递 app 对象
 app.use(router)
 app.use(store)
 app.mount('#app')
