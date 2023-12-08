9c323354a5243b976783f0a1a116073a9c6eb5d8
callie <chzx_dqy@outlook.com>
2023-12-08 17:46:35 +0800

    feat(service): 封装axios实例的拦截器

--- src/service/index.ts
+++ src/service/index.ts
@@ -4,6 +4,24 @@ import { BASE_URL, TIME_OUT } from './request/config'
 
 const ccRequest = new CCRequest({
   baseURL: BASE_URL,
-  timeout: TIME_OUT
+  timeout: TIME_OUT,
+  interceptor: {
+    requestInterceptor: (config) => {
+      console.log('请求成功的拦截')
+
+      return config
+    },
+    requestInterceptorCatch: (err) => {
+      return err
+    },
+
+    responseInterceptor: (err) => {
+      console.log('响应成功的拦截')
+      return err
+    },
+    responseInterceptorCatch: (err) => {
+      return err
+    }
+  }
 })
 export default ccRequest
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,10 +1,37 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+
+//
+interface CCRequestInterceptors<T = AxiosResponse> {
+  requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
+  requestInterceptorCatch?: (error: any) => any
+  responseInterceptor?: (res: AxiosResponse) => AxiosResponse
+  responseInterceptorCatch?: (error: any) => any
+}
+
+//拓展AxiosRequestConfig
+interface CCRequestConfig extends AxiosRequestConfig {
+  interceptor?: CCRequestInterceptors
+}
+
 // 类的封装逻辑更强
 class CCRequest {
   instance: AxiosInstance
-  constructor(config: AxiosRequestConfig) {
+  interceptors?: CCRequestInterceptors
+  constructor(config: CCRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptor
+
+    //应用实例的请求拦截器
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    //应用实例的响应拦截器
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
   }
   request(config: AxiosRequestConfig): void {
     this.instance.request(config).then((res) => {
